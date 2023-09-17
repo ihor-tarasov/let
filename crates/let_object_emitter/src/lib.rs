1463d2a29b5dddd82d500cdfeefda7621fb0db78
ihor-tarasov <mov.ax.38@gmail.com>
2023-09-18 00:18:32 +0300

    Move Resolver to separate crate

--- crates/let_object_emitter/src/lib.rs
+++ crates/let_object_emitter/src/lib.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, fmt};
+use std::fmt;
 
 pub use let_emitter::Emitter;
 
@@ -15,141 +15,10 @@ impl<'a> fmt::Display for Slice<'a> {
     }
 }
 
-struct LabelInfo {
-    address: Option<u64>,
-    links: Vec<u64>,
-}
-
-struct Resolver {
-    labels: HashMap<Box<[u8]>, LabelInfo>,
-    indexes: HashMap<u64, LabelInfo>,
-}
-
-impl Resolver {
-    fn new() -> Self {
-        Self {
-            labels: HashMap::new(),
-            indexes: HashMap::new(),
-        }
-    }
-
-    fn push_label_name(&mut self, name: &[u8], address: u64) -> let_result::Result {
-        if let Some(label) = self.labels.get_mut(name) {
-            if let Some(label_address) = &mut label.address {
-                *label_address = address;
-                Ok(())
-            } else {
-                let_result::raise!("Symbol \"{}\" already present.", Slice(name))
-            }
-        } else {
-            self.labels.insert(
-                Vec::from(name).into_boxed_slice(),
-                LabelInfo {
-                    address: Some(address),
-                    links: Vec::new(),
-                },
-            );
-            Ok(())
-        }
-    }
-
-    fn push_label_index(&mut self, index: u64, address: u64) -> let_result::Result {
-        if let Some(label) = self.indexes.get_mut(&index) {
-            if label.address.is_some() {
-                let_result::raise!("Indexed label \"{}\" already present.", index)
-            } else {
-                label.address = Some(address);
-                Ok(())
-            }
-        } else {
-            self.indexes.insert(
-                index,
-                LabelInfo {
-                    address: Some(address),
-                    links: Vec::new(),
-                },
-            );
-            Ok(())
-        }
-    }
-
-    fn push_link_name(&mut self, name: &[u8], address: u64) {
-        if let Some(label) = self.labels.get_mut(name) {
-            label.links.push(address);
-        } else {
-            self.labels.insert(
-                Vec::from(name).into_boxed_slice(),
-                LabelInfo {
-                    address: None,
-                    links: vec![address],
-                },
-            );
-        }
-    }
-
-    fn push_link_index(&mut self, index: u64, address: u64) {
-        if let Some(label) = self.indexes.get_mut(&index) {
-            label.links.push(address);
-        } else {
-            self.indexes.insert(
-                index,
-                LabelInfo {
-                    address: None,
-                    links: vec![address],
-                },
-            );
-        }
-    }
-
-    fn resolve<W>(&mut self, write: &mut W) -> let_result::Result
-    where
-        W: std::io::Write + std::io::Seek,
-    {
-        for label in self.indexes.values() {
-            if let Some(address) = label.address {
-                for link in label.links.iter().cloned() {
-                    write.seek(std::io::SeekFrom::Start(link as u64))?;
-                    write.write_all(&address.to_be_bytes())?;
-                }
-            }
-        }
-        self.indexes.clear();
-        for label in self.labels.values_mut() {
-            if let Some(address) = label.address {
-                for link in label.links.iter().cloned() {
-                    write.seek(std::io::SeekFrom::Start(link as u64))?;
-                    write.write_all(&address.to_be_bytes())?;
-                }
-                label.links.clear();
-            }
-        }
-        Ok(())
-    }
-
-    fn save_labels<W>(&self, write: &mut W) -> let_result::Result
-    where
-        W: std::io::Write,
-    {
-        for (name, label) in self.labels.iter() {
-            write!(write, "{}", Slice(name))?;
-            if let Some(address) = label.address {
-                write!(write, " {address}")?;
-            } else {
-                write!(write, " None")?;
-            }
-            for link in label.links.iter() {
-                write!(write, " {link}")?;
-            }
-            writeln!(write)?;
-        }
-        Ok(())
-    }
-}
-
 pub struct ObjectEmitter<W, M> {
     write: W,
     meta: M,
-    resolver: Resolver,
+    resolver: let_resolver::Resolver,
 }
 
 impl<O, M> ObjectEmitter<O, M> {
@@ -157,7 +26,7 @@ impl<O, M> ObjectEmitter<O, M> {
         Self {
             write,
             meta,
-            resolver: Resolver::new(),
+            resolver: let_resolver::Resolver::new(),
         }
     }
 }
